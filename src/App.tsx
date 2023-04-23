8dccb67c4ca9b814a40387896f5931b884874ed0
sergii gorbatyi <se7rgo@gmail.com>
2023-04-23 10:38:08 -0700

    added mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Main from "./routes/main";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 
 function App() {
+  const some = null;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Main</Link>
+            </li>
+            <li></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/routes/main/Main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Main() {
+  return (
+    <div className="Main">
+        <p>
+         Main
+        </p>
+    
+    </div>
+  );
+}
+
+export default Main;
